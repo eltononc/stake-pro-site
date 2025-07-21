f67d97b9e5f1d3e5e4f1eaed8b1834d8d8d554d8
eltononc <elton.ti@grupointegrado.br>
2025-07-21 01:06:47 +0000

    Otimiza a inicialização do Firebase em todas as funções

--- netlify/functions/users-create.js
+++ netlify/functions/users-create.js
@@ -1,17 +1,23 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
-
-const app = initializeApp(firebaseConfig);
+// --- Firebase Initialization Otimizada ---
+let app;
+if (!getApps().length) {
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+  };
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
 const db = getFirestore(app);
+// --- Fim da Firebase Initialization Otimizada ---
 
 exports.handler = async (event, context) => {
   if (event.httpMethod !== 'POST') {
@@ -28,11 +34,10 @@ exports.handler = async (event, context) => {
       };
     }
 
-    // Adiciona um novo documento com um ID gerado automaticamente
     const docRef = await addDoc(collection(db, "users"), {
       name: newUser.name,
       email: newUser.email,
-      password: newUser.password, // Nota: Em uma aplicação real, a senha deve ser criptografada.
+      password: newUser.password,
       avatar: newUser.avatar || null,
     });
 
